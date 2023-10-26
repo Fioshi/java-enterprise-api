9976e74f12d81d24db99febbf8d8fd62432b82f1
Fioshi <guilhermefiochi@outlook.com>
2023-10-26 12:25:56 -0300

    Feitas configurações de Cors para funcionamento adequado

---
+++ src/main/java/br/com/empresa/Empresa/infra/configurations/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package br.com.empresa.Empresa.infra.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
+    }
+}
+
