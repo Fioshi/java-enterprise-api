759ccb38c851b801d420c578ed2be571e536a179
Fioshi <guilhermefiochi@outlook.com>
2023-10-26 12:27:00 -0300

    Implementada busca com cpf

--- src/main/java/br/com/empresa/Empresa/controller/FuncionarioController.java
+++ src/main/java/br/com/empresa/Empresa/controller/FuncionarioController.java
@@ -3,14 +3,19 @@ package br.com.empresa.Empresa.controller;
 import br.com.empresa.Empresa.domain.departamento.DepartamentoRepository;
 import br.com.empresa.Empresa.domain.funcionario.*;
 import br.com.empresa.Empresa.domain.funcionario.validacoes.ValidadorCadastroFuncionario;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/funcionario")
@@ -33,8 +38,7 @@ public class FuncionarioController {
     @Transactional
     public ResponseEntity cadastrar(@RequestBody @Validated DadosCadastroFuncionario dados,
                                     UriComponentsBuilder uriBuilder) {
-
-        validadores.forEach(v->v.validar(dados));
+        validadores.forEach(v -> v.validar(dados));
 
         var departamento = departamentoRepository.getReferenceById(dados.departamento());
         var funcionario = new Funcionario(dados, departamento);
@@ -57,9 +61,34 @@ public class FuncionarioController {
 
     @PutMapping
     @Transactional
-    public ResponseEntity atualizar(DadosAtualizacaoFuncionario dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoFuncionario dados) {
         var funcionario = funcionarioRepository.getReferenceById(dados.id());
         funcionario.atualizar(dados);
-        return ResponseEntity.ok(new DadosAtualizacaoFuncionario(funcionario));
+        return ResponseEntity.ok(new DadosDetalhamentoFuncionario(funcionario));
     }
+
+    @GetMapping
+    public ResponseEntity<Stream<DadosDetalhamentoFuncionario>> listar(@PageableDefault(size = 4,
+            sort = {"nome"}) Pageable pageable) {
+        var page = funcionarioRepository.findAllByStatusTrue(pageable).stream()
+                .map(DadosDetalhamentoFuncionario::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/busca")
+    public ResponseEntity buscarFiltrado(@RequestParam("keyword") String keyword){
+        var funcionario = funcionarioRepository.findByCpfContaining(keyword);
+        List<DadosDetalhamentoFuncionario>list = new LinkedList<>();
+        funcionario.forEach( f -> {
+            list.add(new DadosDetalhamentoFuncionario(f));
+        });
+        return ResponseEntity.ok(list);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity buscar(@PathVariable Long id) {
+        var funcionario = funcionarioRepository.getReferenceById(id);
+       return ResponseEntity.ok(new DadosDetalhamentoFuncionario(funcionario));
+   }
 }
