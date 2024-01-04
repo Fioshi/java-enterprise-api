894d1f9c33e0b0f45dfc5b7c0251001cc52065e1
Fioshi <guilhermefiochi@outlook.com>
2024-01-04 13:57:10 -0300

    Classes de controle de autenticação de ususarios

---
+++ src/main/java/br/com/empresa/Empresa/controller/AutenticacaoController.java
@@ -0,0 +1,27 @@
+package br.com.empresa.Empresa.controller;
+
+import br.com.empresa.Empresa.domain.usuario.DadosAutenticacao;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authenticate = authenticationManager.authenticate(token);
+        return ResponseEntity.ok().build();
+    }
+}
