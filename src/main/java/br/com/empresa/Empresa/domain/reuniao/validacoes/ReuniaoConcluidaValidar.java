ce204ad42075c57a5e861e5f91e186aa0b071137
Fioshi <guilhermefiochi@outlook.com>
2024-01-04 13:55:13 -0300

    Codigo refatorado para melhor leitura e bug fixed, alteração para execução diaria

--- src/main/java/br/com/empresa/Empresa/domain/reuniao/validacoes/ReuniaoConcluidaValidar.java
+++ src/main/java/br/com/empresa/Empresa/domain/reuniao/validacoes/ReuniaoConcluidaValidar.java
@@ -4,6 +4,7 @@ import br.com.empresa.Empresa.domain.funcionario.Funcionario;
 import br.com.empresa.Empresa.domain.funcionario.FuncionarioRepository;
 import br.com.empresa.Empresa.domain.reuniao.Reuniao;
 import br.com.empresa.Empresa.domain.reuniao.ReuniaoRepository;
+import br.com.empresa.Empresa.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -22,22 +23,28 @@ public class ReuniaoConcluidaValidar {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
-    @Scheduled(fixedDelay = 50000)
+    @Autowired
+    private EmailService emailService;
+
+    @Scheduled(fixedRate = 24 * 60 * 60 * 1000)
     @Transactional
     public void retirar() {
 
         var agora = LocalDateTime.now();
         var reuniaoList = reuniaoRepository.findAllByStatusTrue();
-        var funcionarioList = funcionarioRepository.findAll();
 
         reuniaoList.forEach(r -> {
-            if (Duration.between(agora, r.getHorario()).toMinutes() < 0) {
+
+            r.getFuncionarios().forEach( f -> {
+                if (Duration.between(agora, r.getHorario()).toDays() <= 1) {
+                    System.out.println(Duration.between(agora, r.getHorario()).toDays());
+                    emailService.enviarEmail(f, "lembrete");
+                }
+            });
+
+            if (Duration.between(agora, r.getHorario()).toMinutes() <= 0) {
                 r.excluir();
-                funcionarioList.forEach(fun -> {
-                    if (fun.getReuniao().equals(r)) {
-                        fun.reuniaoConcluida();
-                    }
-                });
+                r.getFuncionarios().forEach(Funcionario::reuniaoConcluida);
             }
         });
     }
